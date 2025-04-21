694b5ec09171c318e7a5b958565b8d658e85eece
Kirisha <tharmkir@students.zhaw.ch>
2025-04-21 10:38:39 +0200

    buttons

--- Morphaway/Home.py
+++ Morphaway/Home.py
@@ -22,9 +22,9 @@ def history_page():
     st.write("Hier kannst du die Historie einsehen.")
     st.write("Funktionalität wird noch hinzugefügt.")
 
-# Sidebar-Navigation mit Dropdown-Menü
+# Sidebar-Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Seite auswählen", ["Home", "Cell Counter", "History"])
+page = st.sidebar.radio("Seite auswählen", ["Home", "Cell Counter", "History"])
 
 # Inhalte basierend auf der Auswahl anzeigen
 if page == "Home":
@@ -33,4 +33,28 @@ elif page == "Cell Counter":
     cell_counter_page()
 elif page == "History":
     history_page()
-    
+
+    # Navigation mit Buttons im Hauptbereich
+if "current_page" not in st.session_state:
+    st.session_state.current_page = "Home"  # Standardseite
+
+# Buttons für die Navigation
+st.write("### Navigation")
+col1, col2, col3 = st.columns(3)
+with col1:
+    if st.button("Home"):
+        st.session_state.current_page = "Home"
+with col2:
+    if st.button("Cell Counter"):
+        st.session_state.current_page = "Cell Counter"
+with col3:
+    if st.button("History"):
+        st.session_state.current_page = "History"
+
+# Inhalte basierend auf der aktuellen Seite anzeigen
+if st.session_state.current_page == "Home":
+    home_page()
+elif st.session_state.current_page == "Cell Counter":
+    cell_counter_page()
+elif st.session_state.current_page == "History":
+    history_page()
