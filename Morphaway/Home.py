29c84e6021c07d23afdaa262262afa1f2ff0ffe9
Kirisha <tharmkir@students.zhaw.ch>
2025-04-21 10:02:32 +0200

    Quervernetzung der Seiten

--- Morphaway/Home.py
+++ Morphaway/Home.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import Home
+import Cell-counter
 
-st.title("Hallo, Streamlit!")
-st.write("Dies ist eine einfache Streamlit-App.")
+# Query-Parameter auslesen
+query_params = st.experimental_get_query_params()
+page = query_params.get("page", ["home"])[0]
+
+# Navigation basierend auf der Seite
+if page == "home":
+    Home
+elif page == "cell-counter":
+    Cell-counter
