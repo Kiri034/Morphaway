e717623dc886345785d8f4513da74bc224d3c9e0
Kirisha <tharmkir@students.zhaw.ch>
2025-04-21 10:39:25 +0200

    falsche darstellung

--- Morphaway/Home.py
+++ Morphaway/Home.py
@@ -34,27 +34,4 @@ elif page == "Cell Counter":
 elif page == "History":
     history_page()
 
-    # Navigation mit Buttons im Hauptbereich
-if "current_page" not in st.session_state:
-    st.session_state.current_page = "Home"  # Standardseite
 
-# Buttons für die Navigation
-st.write("### Navigation")
-col1, col2, col3 = st.columns(3)
-with col1:
-    if st.button("Home"):
-        st.session_state.current_page = "Home"
-with col2:
-    if st.button("Cell Counter"):
-        st.session_state.current_page = "Cell Counter"
-with col3:
-    if st.button("History"):
-        st.session_state.current_page = "History"
-
-# Inhalte basierend auf der aktuellen Seite anzeigen
-if st.session_state.current_page == "Home":
-    home_page()
-elif st.session_state.current_page == "Cell Counter":
-    cell_counter_page()
-elif st.session_state.current_page == "History":
-    history_page()
