9b72e36554ca1782bbc6ca39346c308d9469f272
naulichi <chiara@tschuba.ch>
2025-06-01 13:15:19 +0200

    Home 2

--- Home.py
+++ Home.py
@@ -45,9 +45,9 @@ name = st.session_state.get("name")
 
 if name:
     first_name = name.split()[0]
-    st.write(f"Willkommen {first_name} bei Morphaway!")
+    st.write(f"Herzlich Willkommen {first_name} bei Morphaway!")
 else:
-    st.write("Willkommen bei Morphaway!")
+    st.write("Herzlich Willkommen bei Morphaway!")
 st.write("Morphaway ist eine einfache und übersichtliche Variante, um ein Blutbild auszuzählen.")
 st.write("Durch eine individuelle Auswahl der Differenzierungsoptionen ist unsere App sehr nutzerfreundlich aufgebaut und kann so in verschiedene Situationen angewendet werden. Egal, ob du dein:e Dozent:in mit einer schnellen 50er-Leukozytendifferenzierung beeindrucken möchtest, 100 Leukozyten für eine genauere Bestimmung brauchst oder ganze 200 Leukozyten zur Labordiagnostik differenzieren möchtest - Morphaway bietet dir die Unterstützung.")
 st.write('Die Werte der ausgezählten Zellen findest du prozentual sowie grafisch dargestellt unter "History". Durch dein Nutzerlogin kannst du sie jederzeit als PDF-Datei herunterladen.')
